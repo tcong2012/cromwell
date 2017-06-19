0b451e21fbdde6eb07ea8e99d3b43e85c6246cfa
geoffjentry <geoffjentry@gmail.com>
2017-06-19 12:11:28 -0400

    oops

--- engine/src/main/scala/cromwell/server/CromwellServer.scala
+++ engine/src/main/scala/cromwell/server/CromwellServer.scala
@@ -7,6 +7,7 @@ import akka.http.scaladsl.server.Directives._
 
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
+import cromwell.core.Dispatcher.EngineDispatcher
 import cromwell.webservice.{AkkaHttpService, SwaggerService}
 import cromwell.webservice.WrappedRoute._
 import net.ceedubs.ficus.Ficus._
