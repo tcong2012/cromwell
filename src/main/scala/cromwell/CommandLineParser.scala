fd3dfb15a03ad889443a7b7fb5402357f209070a
Miguel Covarrubias <mcovarr@broadinstitute.org>
2017-06-19 22:21:44 -0400

    known broken interim refactor

--- src/main/scala/cromwell/CommandLineParser.scala
+++ src/main/scala/cromwell/CommandLineParser.scala
@@ -2,6 +2,7 @@ package cromwell
 
 import com.typesafe.config.ConfigFactory
 import cromwell.core.path.{DefaultPathBuilder, Path}
+import scopt.OptionParser
 
 object CommandLineParser extends App {
 
@@ -22,6 +23,8 @@ object CommandLineParser extends App {
 
   lazy val cromwellVersion = ConfigFactory.load("cromwell-version.conf").getConfig("version").getString("cromwell")
 
+  case class ParserAndCommand(parser: OptionParser[CommandLineArguments], command: Option[Command])
+
 //  Usage: cromwell [server|run] [options] <args>...
 //
 //    --help                   Cromwell - Lord Protector / Workflow Execution Engine
@@ -41,60 +44,62 @@ object CommandLineParser extends App {
 //    -m, --metadata-output-path <value>
 //                             An optional file path to output metadata.
 
-  val parser = new scopt.OptionParser[CommandLineArguments]("cromwell") {
-    head("cromwell", cromwellVersion)
+  def buildParser(): scopt.OptionParser[CommandLineArguments] = {
+    new scopt.OptionParser[CommandLineArguments]("cromwell") {
+      head("cromwell", cromwellVersion)
 
-    help("help").text("Cromwell - Lord Protector / Workflow Execution Engine")
+      help("help").text("Cromwell - Lord Protector / Workflow Execution Engine")
 
-    version("version")
+      version("version")
 
-    cmd("server").action((_, c) => c.copy(command = Option(Server))).text(
-      "Starts a web server on port 8000.  See the web server documentation for more details about the API endpoints.")
+      cmd("server").action((_, c) => c.copy(command = Option(Server))).text(
+        "Starts a web server on port 8000.  See the web server documentation for more details about the API endpoints.")
 
-    cmd("run").
-      action((_, c) => c.copy(command = Option(Run))).
-      text("Run the workflow locally and print out the outputs in JSON format.").
-      children(
-        arg[String]("workflow-source").text("Workflow source file.").required().
-          action((s, c) => c.copy(workflowSource = Option(DefaultPathBuilder.get(s)))),
-        opt[String]('i', "inputs").text("Workflow inputs file.").
-          action((s, c) =>
-            c.copy(workflowInputs = Option(DefaultPathBuilder.get(s)))),
-        opt[String]('o', "options").text("Workflow options file.").
-          action((s, c) =>
-            c.copy(workflowOptions = Option(DefaultPathBuilder.get(s)))),
-        opt[String]('t', "type").text("Workflow type.").
-          action((s, c) =>
-            c.copy(workflowType = Option(s))),
-        opt[String]('v', "type-version").text("Workflow type version.").
-          action((s, c) =>
-            c.copy(workflowTypeVersion = Option(s))),
-        opt[String]('l', "labels").text("Labels file.").
-          action((s, c) =>
-            c.copy(labels = Option(DefaultPathBuilder.get(s)))),
-        opt[String]('p', "imports").text(
-          "A directory to search for WDL file imports, required for imported workflows outside the root directory of the Cromwell project.").
-          action((s, c) =>
-            c.copy(imports = Option(DefaultPathBuilder.get(s)))),
-        opt[String]('m', "metadata-output-path").text(
-          "An optional file path to output metadata.").
-          action((s, c) =>
-            c.copy(metadataOutputPath = Option(DefaultPathBuilder.get(s))))
-      )
+      cmd("run").
+        action((_, c) => c.copy(command = Option(Run))).
+        text("Run the workflow locally and print out the outputs in JSON format.").
+        children(
+          arg[String]("workflow-source").text("Workflow source file.").required().
+            action((s, c) => c.copy(workflowSource = Option(DefaultPathBuilder.get(s)))),
+          opt[String]('i', "inputs").text("Workflow inputs file.").
+            action((s, c) =>
+              c.copy(workflowInputs = Option(DefaultPathBuilder.get(s)))),
+          opt[String]('o', "options").text("Workflow options file.").
+            action((s, c) =>
+              c.copy(workflowOptions = Option(DefaultPathBuilder.get(s)))),
+          opt[String]('t', "type").text("Workflow type.").
+            action((s, c) =>
+              c.copy(workflowType = Option(s))),
+          opt[String]('v', "type-version").text("Workflow type version.").
+            action((s, c) =>
+              c.copy(workflowTypeVersion = Option(s))),
+          opt[String]('l', "labels").text("Labels file.").
+            action((s, c) =>
+              c.copy(labels = Option(DefaultPathBuilder.get(s)))),
+          opt[String]('p', "imports").text(
+            "A directory to search for WDL file imports, required for imported workflows outside the root directory of the Cromwell project.").
+            action((s, c) =>
+              c.copy(imports = Option(DefaultPathBuilder.get(s)))),
+          opt[String]('m', "metadata-output-path").text(
+            "An optional file path to output metadata.").
+            action((s, c) =>
+              c.copy(metadataOutputPath = Option(DefaultPathBuilder.get(s))))
+        )
+    }
   }
 
-  private def runCromwell(command: Command, args: CommandLineArguments): Unit = {
-    command match {
+  def runCromwell(args: CommandLineArguments): Unit = {
+    args.command foreach {
       case Run => CromwellEntryPoint.runSingle(args)
       case Server => CromwellEntryPoint.runServer()
     }
   }
 
-  val run = for {
-    config <- parser.parse(args, CommandLineArguments())
-    command <- config.command
-  } yield runCromwell(command, config)
+  val parser = buildParser()
 
-  // parser.parse returns Option[C].  If this is `None` (corresponding to invalid arguments) show usage and exit.
-  run getOrElse parser.showUsageAsError()
+  val parsedArgs = parser.parse(args, CommandLineArguments())
+  parsedArgs match {
+    case Some(pa) => runCromwell(pa)
+    case None => parser.showUsage()
+  }
 }
---
+++ src/test/scala/cromwell/CromwellCommandLineSpec.scala
@@ -0,0 +1,151 @@
+package cromwell
+
+import cromwell.CommandLineParser.{CommandLineArguments, Server}
+import cromwell.core.path.{DefaultPathBuilder, Path}
+import cromwell.util.SampleWdl
+import cromwell.util.SampleWdl.ThreeStep
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+class CromwellCommandLineSpec extends FlatSpec with Matchers with BeforeAndAfter {
+
+  var parser: scopt.OptionParser[CommandLineArguments] = _
+
+  behavior of "CromwellCommandLine"
+
+  before {
+    parser = CommandLineParser.buildParser()
+  }
+
+  it should "fail to parse with no arguments" in {
+    parser.parse(Array.empty[String], CommandLineArguments()) shouldBe None
+  }
+
+  it should "RunServer when specified" in {
+    parser.parse(Array("server"), CommandLineArguments()) shouldBe Some(Server)
+  }
+
+//  it should "UsageAndExit when supplying an argument to server" in {
+//    CromwellCommandLine(List("server", "foo")) shouldBe UsageAndExit
+//  }
+//
+//  it should "UsageAndExit with no arguments to run" in {
+//    CromwellCommandLine(List("run")) shouldBe UsageAndExit
+//  }
+//
+//  it should "fail with too many arguments to run" in {
+//    CromwellCommandLine(List("run", "bork", "bork", "bork", "bork", "bork", "blerg"))
+//  }
+//
+//  it should "VersionAndExit when the `-version` flag is passed" in {
+//    CromwellCommandLine(List("-version")) shouldBe VersionAndExit
+//  }
+//
+//  it should "RunSingle when supplying wdl and inputs" in {
+//    CromwellCommandLine(List("run", ThreeStepWithoutOptions.wdl, ThreeStepWithoutOptions.inputs)) shouldBe a [RunSingle]
+//  }
+//
+//  it should "RunSingle with default inputs when only supplying wdl" in {
+//    val ccl = CromwellCommandLine(List("run", ThreeStepWithoutOptions.wdl)).asInstanceOf[RunSingle]
+//     ccl.sourceFiles.inputsJson shouldBe ThreeStepInputs
+//  }
+//
+//  it should "RunSingle with defaults if you use dashes" in {
+//    val ccl = CromwellCommandLine(List("run", ThreeStepWithoutOptions.wdl, "-", "-", "-")).asInstanceOf[RunSingle]
+//    ccl.sourceFiles.inputsJson shouldBe "{}"
+//    ccl.sourceFiles.workflowOptionsJson shouldBe "{}"
+//  }
+//
+//  it should "RunSingle with options, if passed in" in {
+//    val threeStep = WdlAndInputs(ThreeStep, optionsJson = """{ foobar bad json! }""")
+//    val ccl = CromwellCommandLine(List("run", threeStep.wdl, threeStep.inputs, threeStep.options)).asInstanceOf[RunSingle]
+//    ccl.sourceFiles.workflowOptionsJson shouldBe threeStep.optionsJson
+//  }
+//
+//  it should "fail if inputs path does not exist" in {
+//    val ccl = Try(CromwellCommandLine(List("run", ThreeStepWithoutOptions.wdl, "/some/path/that/doesnt/exit")))
+//    ccl.isFailure shouldBe true
+//    ccl.failed.get.getMessage should include("Inputs does not exist")
+//  }
+//
+//  it should "fail if inputs path is not writeable" in {
+//    val threeStep = WdlAndInputs(ThreeStep)
+//    threeStep.inputsFile setPermissions Set.empty
+//    val ccl = Try(CromwellCommandLine(List("run", threeStep.wdl, threeStep.inputs)))
+//    ccl.isFailure shouldBe true
+//    ccl.failed.get.getMessage should include("Inputs is not readable")
+//  }
+//
+//  it should "fail if metadata path is not writeable" in {
+//    val threeStep = WdlAndInputs(ThreeStep)
+//    threeStep.metadataFile write "foo"
+//    threeStep.metadataFile setPermissions Set.empty
+//    val ccl = Try(CromwellCommandLine(List("run", threeStep.wdl, threeStep.inputs, "-", threeStep.metadata)))
+//    ccl.isFailure shouldBe true
+//    ccl.failed.get.getMessage should include("Unable to write to metadata directory:")
+//  }
+//
+//  it should "run if imports directory is a .zip file" in {
+//    val wdlDir = DefaultPathBuilder.createTempDirectory("wdlDirectory")
+//
+//    val filePassing = DefaultPathBuilder.createTempFile("filePassing", ".wdl", Option(wdlDir))
+//    val fileClobber = DefaultPathBuilder.createTempFile("fileClobber", ".wdl", Option(wdlDir))
+//    filePassing write FilePassingWorkflow.workflowSource()
+//    fileClobber write FileClobber.workflowSource()
+//
+//    val zippedDir = wdlDir.zip()
+//    val zippedPath = zippedDir.pathAsString
+//
+//    val ccl = Try(CromwellCommandLine(List("run", filePassing.pathAsString, "-", "-", "-", zippedPath)))
+//    ccl.isFailure shouldBe false
+//
+//    zippedDir.delete(swallowIOExceptions = true)
+//  }
+}
+
+object CromwellCommandLineSpec {
+  val ThreeStepWithoutOptions = WdlAndInputs(ThreeStep)
+  val ThreeStepInputs = ThreeStepWithoutOptions.inputsFile.contentAsString
+
+  /**
+   * Create a temporary wdl file and inputs for the sampleWdl.
+   * When the various properties are lazily accessed, they are also registered for deletion after the suite completes.
+   */
+  case class WdlAndInputs(sampleWdl: SampleWdl, optionsJson: String = "{}") {
+    // Track all the temporary files we create, and delete them after the test.
+    private var tempFiles = Vector.empty[Path]
+
+    lazy val wdlFile = {
+      val file = DefaultPathBuilder.createTempFile(s"${sampleWdl.name}.", ".wdl")
+      tempFiles :+= file
+      file write sampleWdl.workflowSource()
+    }
+
+    lazy val wdl = wdlFile.pathAsString
+
+    lazy val inputsFile = {
+      val file = wdlFile.swapExt("wdl", "inputs")
+      tempFiles :+= file
+      file write sampleWdl.wdlJson
+    }
+
+    lazy val inputs = inputsFile.pathAsString
+
+    lazy val optionsFile = {
+      val file = wdlFile.swapExt("wdl", "options")
+      tempFiles :+= file
+      file write optionsJson
+    }
+
+    lazy val options = optionsFile.pathAsString
+
+    lazy val metadataFile = {
+      val path = wdlFile.swapExt("wdl", "metadata.json")
+      tempFiles :+= path
+      path
+    }
+
+    lazy val metadata = metadataFile.pathAsString
+
+    def deleteTempFiles() = tempFiles.foreach(_.delete(swallowIOExceptions = true))
+  }
+}
